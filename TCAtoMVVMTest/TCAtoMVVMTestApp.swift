369b3cddd4b1f81ceaad9b034861c7d34a5a976f
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2024-06-11 11:01:53 -0400

    Initial Commit

---
+++ TCAtoMVVMTest/TCAtoMVVMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAtoMVVMTestApp.swift
+//  TCAtoMVVMTest
+//
+//  Created by Jon Duenas on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAtoMVVMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
